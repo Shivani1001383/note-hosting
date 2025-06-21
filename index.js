f3a91c0146431cc557b7262a7e7ba7f0bab12d66
shivani <shivanikpatil001@gmail.com>
2025-06-21 19:49:34 +0530

    static added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("dotenv").config()
 const cors = require("cors")
 const app = express()
 app.use(express.json())
+app.use(express.static("dist"))
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use("/api/note", require("./routes/notes.routes"))
 mongoose.connect(process.env.MONGO_URL)
